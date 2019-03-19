c414dbfe3d125509ce911f6d259a5c7a78c8948d
Dingyj3178 <ll3178292@gmail.com>
2019-03-19 18:34:49 +0900

    raindrop_v2.0

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,25 +1,23 @@
 package raindrops
 
-import "strconv"
+import (
+	"sort"
+	"strconv"
+)
 
-//Convert function convert a number to a string.
+//Convert function convert a number to a string
 func Convert(a int) string {
-	switch {
-	case a%105 == 0:
-		return "PlingPlangPlong"
-	case a%35 == 0:
-		return "PlangPlong"
-	case a%21 == 0:
-		return "PlingPlong"
-	case a%15 == 0:
-		return "PlingPlang"
-	case a%3 == 0:
-		return "Pling"
-	case a%5 == 0:
-		return "Plang"
-	case a%7 == 0:
-		return "Plong"
-	default:
-		return strconv.Itoa(a)
+	result := ""
+	keys := []int{3, 5, 7}
+	factor := map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
+	sort.Ints(keys)
+	for _, f := range keys {
+		if a%f == 0 {
+			result += factor[f]
+		}
 	}
+	if result == "" {
+		result = strconv.Itoa(a)
+	}
+	return result
 }
