be31872b19e46a08315ccc6bdc87fdbb2ebab096
Dingyj3178 <ll3178292@gmail.com>
2019-03-19 18:20:29 +0900

    raindrops change

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,21 +1,25 @@
 package raindrops
 
-import "fmt"
+import "strconv"
 
 //Convert function convert a number to a string
 func Convert(a int) string {
-	result := ""
-	if a%3 == 0 {
-		result += "Pling"
+	switch {
+	case a%105 == 0:
+		return "PlingPlangPlong"
+	case a%35 == 0:
+		return "PlangPlong"
+	case a%21 == 0:
+		return "PlingPlong"
+	case a%15 == 0:
+		return "PlingPlang"
+	case a%3 == 0:
+		return "Pling"
+	case a%5 == 0:
+		return "Plang"
+	case a%7 == 0:
+		return "Plong"
+	default:
+		return strconv.Itoa(a)
 	}
-	if a%5 == 0 {
-		result += "Plang"
-	}
-	if a%7 == 0 {
-		result += "Plong"
-	}
-	if result == "" {
-		return fmt.Sprintf("%d", a)
-	}
-	return result
 }
