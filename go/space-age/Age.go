8e12ae23462d91770bed2b9551f4b144607c6989
Dingyj3178 <ll3178292@gmail.com>
2019-03-19 19:15:21 +0900

    spaceage2.0

--- go/space-age/Age.go
+++ go/space-age/Age.go
@@ -1,34 +1,25 @@
 package space
 
-//Planet provide type planet
+var secondsInOneEarthYear = 31557600.0
+var orbitalPeriodsInEarthYears = map[Planet]float64{
+	"Earth":   1.0,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
 type Planet string
 
-//Age Function calculate second in universe
-func Age(s float64, p Planet) float64 {
-	sp := s / 31557600
-	switch p {
-	case "Earth":
+func Age(seconds float64, planet Planet) float64 {
 
-	case "Mercury":
-		sp = sp / 0.2408467
+	secondsInAYearOnPlanet := secondsInOneEarthYear * orbitalPeriodsInEarthYears[planet]
 
-	case "Venus":
-		sp = sp / 0.61519726
+	years := seconds / secondsInAYearOnPlanet
 
-	case "Mars":
-		sp = sp / 1.8808158
+	return years
 
-	case "Jupiter":
-		sp = sp / 11.862615
-
-	case "Saturn":
-		sp = sp / 29.447498
-
-	case "Uranus":
-		sp = sp / 84.016846
-
-	case "Neptune":
-		sp = sp / 164.79132
-	}
-	return sp
 }
